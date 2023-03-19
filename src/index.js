6c82a6788d843ed56cb54f7d6a217ed1d7d1aa01
Thomas Rose <thomas.rose@ionos.com>
2023-03-19 15:03:05 +0100

    fix react version

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import { createRoot, render } from '@wordpress/element';
+import { render } from '@wordpress/element';
 import domReady from '@wordpress/dom-ready';
 import App from './App';
 
 const id = 'wapuugotchi-app';
 
 domReady( function () {
-	const root = createRoot( document.getElementById( id ) )
-	root.render( <App />);
+	render( <App />, document.getElementById( id ) );
 } );

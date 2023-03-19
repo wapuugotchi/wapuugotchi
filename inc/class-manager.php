f428120910df320f22cff27021e615cb869b9dd8
Thomas Rose <thomas.rose@ionos.com>
2023-03-19 09:47:52 +0100

    prepare for simultaneous coding

--- inc/class-manager.php
+++ inc/class-manager.php
@@ -28,10 +28,8 @@ class Manager {
 		wp_enqueue_script( 'wapuugotchi-shop', plugins_url( 'build/index.js', __DIR__ ), $assets['dependencies'], $assets['version'], true );
 
 		wp_localize_script( 'wapuugotchi-shop', 'wpPluginParam', [
-			// toDo: combine both collections into one, so that the frontend works with one collection
-			// toDo: get collection from $this->get_collection()
-			'unlockedCollection' => json_decode( file_get_contents( \plugin_dir_path( __DIR__ ) . 'config/unlockedCollection.json' ) ),
-			'lockedCollection'   => json_decode( file_get_contents( \plugin_dir_path( __DIR__ ) . 'config/lockedCollection.json' ) ),
+			'unlockedCollection' => $this->get_collection( 'unlockedCollection.json'),
+			'lockedCollection'   => $this->get_collection( 'lockedCollection.json' ),
 			'wapuu'              => json_decode( get_user_meta( get_current_user_id(), 'wapuugotchi', true ) ),
 			'apiUrl'             => get_rest_url( null, 'wapuugotchi' ),
 			'nonce'              => wp_create_nonce( 'wp_rest' ),
@@ -74,7 +72,12 @@ class Manager {
 	/**
 	 * Gets the config. Retrieves it from server if necessary.
 	 */
-	private function get_collection(){
+	private function get_collection( $file ){
+		// toDo: refactor the following code to use the new collection format
+		return  json_decode(
+			file_get_contents( \plugin_dir_path( __DIR__ ) . 'config/' . $file ) 
+		);
+		
 		if (empty( get_transient( 'wapuugotchi_collection' ) ) ) {
 			$this->set_collection();
 		}
@@ -85,11 +88,7 @@ class Manager {
 	/**
 	 * Retrieves the collection from the remote server and sets it as transient.
 	 */
-	private function set_collection() {
-		// $config = file_get_contents( \plugin_dir_path( __DIR__ ) . 'config/unlockedCollection.json' ) ;
-		// set_transient( 'wapuugotchi_collection', $config, 60 * 60 * 24 );
-		// return;
-
+	private function set_collection(  ) {
 		$response = wp_remote_get( 'https://api.wapuugotchi.com/collection.json' );
 		if ( is_wp_error( $response ) ) {
 			return;

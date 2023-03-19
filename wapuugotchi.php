101140c2df6b08e182810faf1bf14744c1b0faf4
Thomas Rose <thomas.rose@ionos.com>
2023-03-19 15:47:00 +0100

    fix things

--- wapuugotchi.php
+++ wapuugotchi.php
@@ -2,13 +2,13 @@
 /**
  * Plugin Name:       Wapuugotchi
  * Description:       Example block scaffolded with Create Block tool.
- * Requires at least: 6.1
+ * Requires at least: 5.9
  * Requires PHP:      7.0
  * Version:           0.1.0
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       wapuugotchi2
+ * Text Domain:       wapuugotchi
  *
  * @package           create-block
  */

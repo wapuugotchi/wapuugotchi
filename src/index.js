ef764b061eb19b32f93934150bd6e2fbaa12cf87
Lars Gersmann <lars.gersmann@gmail.com>
2023-03-20 09:23:53 +0100

    fix: react rending works now also with react <= 18

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { createRoot, render } from '@wordpress/element';
+import { render } from '@wordpress/element';
 import domReady from '@wordpress/dom-ready';
 import App from './App';
 import { create, STORE_NAME } from './store';
@@ -6,7 +6,4 @@ import data from "@wordpress/data";
 
 const id = 'wapuugotchi-app';
 
-domReady( function () {
-	const root = createRoot( document.getElementById( id ) )
-	root.render( <App />);
-} );
+domReady( ()=> render( <App />, document.getElementById( id )));

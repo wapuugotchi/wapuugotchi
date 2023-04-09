5360ef89d030c1d1f1ab4d576da1cc194dcddaa5
DerHerrFeldmann <markofeldmann.development@gmail.com>
2023-04-09 09:58:40 +0200

    update frontend - the page now has a min-width, so that the elements don't lie out of the view.

--- src/components/ShowRoom.js
+++ src/components/ShowRoom.js
@@ -93,6 +93,7 @@ const ShowRoom = (props) => {
 
 	return (
 		<div className="wapuu_show_room">
+			{/*		changes to the ViewBox can destroy the animations.		*/}
 			<svg xmlns="http://www.w3.org/2000/svg" x="0" y="0" version="1.1" viewBox="140 100 700 765"
 					 dangerouslySetInnerHTML={{__html: svg}}></svg>
 		</div>

26dd4cc0298603c4501ae5367520defce50c305c
Lars Gersmann <lars.gersmann@cm4all.com>
2023-04-18 09:31:14 +0200

    refactor: moved purchasing an item into store

--- src/components/category-item.js
+++ src/components/category-item.js
@@ -1,8 +1,7 @@
-import { useCallback, useState } from "@wordpress/element";
+import { useCallback } from "@wordpress/element";
 
 import { STORE_NAME } from "../store";
 import { useSelect, dispatch } from "@wordpress/data";
-import apiFetch from "@wordpress/api-fetch";
 
 import priceTag from "./category-item-pricetag.svg";
 import "./category-item.scss";
@@ -18,32 +17,14 @@ export default function CategoryItem({ selectedCategory, item }) {
 		};
 	});
 
-	const shopping = async (item_data, category) => {
-		const success = apiFetch({
-			path: `${restBase}/purchases`,
-			method: 'POST',
-			data: {
-				item: {
-					key: item_data.meta.key,
-					price: item_data.meta.price,
-				}},
-		});
-
-		return success;
-	}
-
-	const __handleItem = async () => {
+	const __handleItem = () => {
 		const data_key = item.meta.key;
 
 		if (items[selectedCategory] && items[selectedCategory][data_key]) {
 			let item_data = items[selectedCategory][data_key];
 			if ( item_data.meta?.price > 0 ) {
 				if( item_data.meta.price <= balance ) {
-					if(await shopping(item_data, selectedCategory)) {
-						dispatch(STORE_NAME).setBalance(balance - item_data.meta.price);
-						item_data.meta.price = 0;
-						dispatch(STORE_NAME).setItems(items);
-					}
+					dispatch(STORE_NAME).purchaseItem(item_data);
 				}
 			} else {
 				const category_data = wapuu.char[selectedCategory];

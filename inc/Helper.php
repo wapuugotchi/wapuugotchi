61008ddcd92706ba947bd32e34844e4d1fea4c1c
DerHerrFeldmann <markofeldmann.development@gmail.com>
2023-07-24 23:50:03 +0200

    fix all phpcs issues for Helper.php

--- inc/Helper.php
+++ inc/Helper.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * The Helper Class.
+ *
+ * @package WapuuGotchi
+ */
 
 namespace Wapuugotchi\Wapuugotchi;
 
@@ -9,6 +14,9 @@ if ( ! defined( 'ABSPATH' ) ) :
 	exit();
 endif; // No direct access allowed.
 
+/**
+ * Class Helper
+ */
 class Helper {
 
 	const COLLECTION_API_URL   = 'https://api.wapuugotchi.com/collection';
@@ -26,18 +34,24 @@ class Helper {
 	 *
 	 * @return string
 	 */
-	static function get_rest_api() {
+	public static function get_rest_api() {
 		$api      = get_rest_url( null, Api::REST_BASE );
 		$find     = 'wp-json';
 		$position = strpos( $api, $find );
-		if ( $position === false ) {
+		if ( false === $position ) {
 			return $api;
 		}
 
 		return substr( $api, $position + strlen( $find ) );
 	}
 
-	static function get_seconds_left_until_tomorrow() {
+	/**
+	 * Get the seconds who are left until tomorrow 00:00
+	 *
+	 * @return int
+	 * @throws \Exception If something went wrong.
+	 */
+	public static function get_seconds_left_until_tomorrow() {
 		$timezone = new DateTimeZone( wp_timezone_string() );
 
 		$today    = new DateTime( 'now', $timezone );
@@ -46,7 +60,12 @@ class Helper {
 		return $tomorrow->getTimestamp() - $today->getTimestamp();
 	}
 
-	static function get_items() {
+	/**
+	 * Get all wearable items.
+	 *
+	 * @return int
+	 */
+	public static function get_items() {
 		$items = \get_transient( 'wapuugotchi_items' );
 		if ( is_array( $items ) && ! empty( $items ) ) {
 			return reset( $items );

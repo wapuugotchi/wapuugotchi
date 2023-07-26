b34d18a7fd4d8bc0d6a82421ba438ef915735552
DerHerrFeldmann <markofeldmann.development@gmail.com>
2023-07-26 23:59:30 +0200

    fix all phpcs issues for QuestDate.php

--- inc/tasks/QuestDate.php
+++ inc/tasks/QuestDate.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * The QuestDate Class.
+ *
+ * @package WapuuGotchi
+ */
 
 namespace Wapuugotchi\Wapuugotchi;
 
@@ -6,14 +11,25 @@ if ( ! defined( 'ABSPATH' ) ) :
 	exit();
 endif; // No direct access allowed.
 
+/**
+ * Class QuestDate
+ */
 class QuestDate {
 
-
+	/**
+	 * "Constructor" of the class
+	 */
 	public function __construct() {
 		add_filter( 'wapuugotchi_quest_filter', array( $this, 'add_wapuugotchi_filter' ) );
-		// add_filter('admin_init', array($this, 'add_wapuugotchi_filter'));
 	}
 
+	/**
+	 * Initialization filter for QuestDate
+	 *
+	 * @param array $quests Array of quest objects.
+	 *
+	 * @return array|Quest[]
+	 */
 	public function add_wapuugotchi_filter( $quests ) {
 		$default_quest = array(
 			new \Wapuugotchi\Wapuugotchi\Quest( 'login_1', null, __( 'Log in on 10 different days', 'wapuugotchi' ), __( 'Nice, you logged in for 10 consecutive days!', 'wapuugotchi' ), 'success', 100, 1, 'Wapuugotchi\Wapuugotchi\QuestDate::always_true', 'Wapuugotchi\Wapuugotchi\QuestDate::login_completed_1' ),
@@ -26,11 +42,20 @@ class QuestDate {
 		return array_merge( $default_quest, $quests );
 	}
 
-	// Posts
+	/**
+	 * Get true.
+	 *
+	 * @return true
+	 */
 	public static function always_true() {
 		return true;
 	}
 
+	/**
+	 * Check completion requirement.
+	 *
+	 * @return bool
+	 */
 	public static function login_completed_1() {
 		$quest_meta = self::count_days();
 		if ( $quest_meta['day_count']['days'] >= 10 ) {
@@ -40,6 +65,11 @@ class QuestDate {
 		return false;
 	}
 
+	/**
+	 * Check completion requirement.
+	 *
+	 * @return bool
+	 */
 	public static function login_completed_2() {
 		$quest_meta = self::count_days();
 		if ( $quest_meta['day_count']['days'] >= 20 ) {
@@ -49,6 +79,11 @@ class QuestDate {
 		return false;
 	}
 
+	/**
+	 * Check completion requirement.
+	 *
+	 * @return bool
+	 */
 	public static function login_completed_3() {
 		$quest_meta = self::count_days();
 		if ( $quest_meta['day_count']['days'] >= 30 ) {
@@ -58,6 +93,11 @@ class QuestDate {
 		return false;
 	}
 
+	/**
+	 * Check completion requirement.
+	 *
+	 * @return bool
+	 */
 	public static function login_completed_4() {
 		$quest_meta = self::count_days();
 		if ( $quest_meta['day_count']['days'] >= 40 ) {
@@ -67,6 +107,11 @@ class QuestDate {
 		return false;
 	}
 
+	/**
+	 * Check completion requirement.
+	 *
+	 * @return bool
+	 */
 	public static function login_completed_5() {
 		$quest_meta = self::count_days();
 		if ( $quest_meta['day_count']['days'] >= 50 ) {
@@ -76,7 +121,11 @@ class QuestDate {
 		return false;
 	}
 
-
+	/**
+	 * Count days and update in user meta.
+	 *
+	 * @return array|array[]|mixed
+	 */
 	private static function count_days() {
 		$quest_meta = get_user_meta( get_current_user_id(), 'wapuugotchi_quest_meta__alpha', true );
 		if ( ! is_array( $quest_meta )
@@ -105,8 +154,13 @@ class QuestDate {
 		return $quest_meta;
 	}
 
-
-	// helper
+	/**
+	 * Check if one of the given plugins is active.
+	 *
+	 * @param array $list List of recommended plugins.
+	 *
+	 * @return bool
+	 */
 	private static function is_active_plugin_in_list( $list ) {
 		if ( ! is_array( $list ) ) {
 			return false;

668435824e590c09afa33a3603db9c302a805882
mfeldmann <marko.feldmann@ionos.com>
2023-11-29 23:26:41 +0100

    first click events

--- src/components/onboarding/guide-footer.js
+++ src/components/onboarding/guide-footer.js
@@ -1,10 +1,15 @@
 import './guide-footer.scss';
 
 export default function GuideFooter() {
+
+	const guideNext = async () => {
+		console.log('Next step!!!!!')
+	};
+
 	return (
 		<>
 			<div className="wapuugotchi_onboarding_guide__footer">
-				<button className="components-button">Next</button>
+				<button className="components-button" onClick={ guideNext }>Next</button>
 			</div>
 		</>
 	);
--- src/components/onboarding/guide-header.js
+++ src/components/onboarding/guide-header.js
@@ -3,10 +3,15 @@ import closing_guide from './guide-header-close.svg';
 
 
 export default function GuideHeader() {
+
+	const guideClose = async () => {
+		console.log('close guide')
+	};
+
 	return (
 		<>
 			<div className="wapuugotchi_onboarding_guide__headline">
-				<button type="button" className="components-button has-icon" aria-label="Close">
+				<button type="button" className="components-button has-icon" aria-label="Close" onClick={ guideClose }>
 					<img src={ closing_guide }  alt="closing button"/>
 				</button>
 			</div>

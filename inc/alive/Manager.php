4f6d61d8847dd200441eeaafa64ed535182c6c64
DerHerrFeldmann <markofeldmann.development@gmail.com>
2024-05-08 23:25:57 +0200

    implement animations extraction function

---
+++ inc/alive/Manager.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The Manager Class.
+ *
+ * @package WapuuGotchi
+ */
+
+namespace Wapuugotchi\Alive;
+
+use Wapuugotchi\Alive\Handler\AnimationHandler;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit();
+}
+
+/**
+ * Class Manager
+ */
+class Manager {
+	/**
+	 * "Constructor" of this Class
+	 */
+	public function __construct() {
+		\add_filter( 'wapuugotchi_avatar', array( AnimationHandler::class, 'extract_animations' ), PHP_INT_MAX, 1);
+		\add_action( 'animations_extracted', array( $this, 'add_animations' ) );
+	}
+
+	/**
+	 * Init plugin.
+	 *
+	 * @return void
+	 */
+	public function add_animations( $animations ) {
+		\add_action( 'admin_enqueue_scripts', function() use ( $animations ) {
+			/*************** DumpDebugDie ***************/
+			echo '<pre>';
+			wp_die(var_dump($animations));
+			echo '</pre>';
+			/*************** DumpDebugDie ***************/
+		}, 20 );
+
+	}
+}
---
+++ inc/alive/handler/AnimationHandler.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The AnimationHandler Class.
+ *
+ * @package WapuuGotchi
+ */
+
+namespace Wapuugotchi\Alive\Handler;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit();
+}
+
+/**
+ * Handles all animation related tasks.
+ */
+class AnimationHandler {
+
+	/**
+	 * remove all animations from avatar.
+	 * @param $avatar
+	 *
+	 * @return array|mixed
+	 */
+	public static function extract_animations( $avatar ) {
+		if ( false === $avatar ) {
+			return $avatar;
+		}
+
+		$domDocument = new \DOMDocument();
+		$domDocument->loadXML( $avatar );
+
+		if ( empty( $domDocument->textContent) ) {
+			return $avatar;
+		}
+
+		$styleElements = $domDocument->getElementsByTagName( 'style' );
+		if ( false === $styleElements instanceof \DOMNodeList || 0 === $styleElements->length ) {
+			return $avatar;
+		}
+
+		$animations = array();
+		foreach ( $styleElements as $styleElement ) {
+			$animations[] = $styleElement->nodeValue;
+		}
+
+		do_action('animations_extracted', $animations);
+
+		// Remove all style elements
+		while ( $styleElement = $styleElements->item( 0 ) ) {
+			$styleElement->parentNode->removeChild( $styleElement );
+		}
+
+		return $domDocument->saveHTML();
+	}
+
+}

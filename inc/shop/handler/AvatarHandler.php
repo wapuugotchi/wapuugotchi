949fa137f6f4d6e53e6071b43119ecd26d130a5b
Angelo Cali <angelo.cali@ionos.com>
2024-05-12 18:28:42 +0200

    Refactor avatar handler

--- inc/shop/handler/AvatarHandler.php
+++ inc/shop/handler/AvatarHandler.php
@@ -16,21 +16,36 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class AvatarHandler {
 	/**
+	 * The key for the avatar configuration user meta.
+	 *
+	 * @var string
+	 */
+	const AVATAR_CONFIG_KEY = 'wapuugotchi_avatar';
+	/**
+	 * The key for the avatar svg user meta.
+	 *
+	 * @var string
+	 */
+	const AVATAR_SVG_KEY = 'wapuugotchi_shop_svg';
+
+	/**
 	 * Get the avatar configuration of the current user
 	 *
 	 * @return array
 	 */
 	public static function get_avatar_config() {
-		if ( empty( \get_user_meta( \get_current_user_id(), 'wapuugotchi_avatar', false ) ) ) {
-			$config = file_get_contents( \plugin_dir_path( __DIR__ ) . 'assets/avatar.json' );
-			\update_user_meta(
-				\get_current_user_id(),
-				'wapuugotchi_avatar',
-				json_decode( $config, true )
-			);
+		$config = \get_user_meta( \get_current_user_id(), 'wapuugotchi_avatar', true );
+		if ( ! empty( $config ) ) {
+			return $config;
 		}
 
-		return \get_user_meta( \get_current_user_id(), 'wapuugotchi_avatar', true );
+		$config = \json_decode(
+			\file_get_contents( \plugin_dir_path( __DIR__ ) . 'assets/avatar.json' ),
+			true
+		);
+		self::update( self::AVATAR_CONFIG_KEY, $config );
+
+		return $config;
 	}
 
 	/**
@@ -41,13 +56,7 @@ class AvatarHandler {
 	 * @return bool
 	 */
 	public static function update_avatar_config( $data ) {
-		\update_user_meta(
-			\get_current_user_id(),
-			'wapuugotchi_avatar',
-			$data
-		);
-
-		return true;
+		return self::update( self::AVATAR_CONFIG_KEY, $data );
 	}
 
 	/**
@@ -56,7 +65,7 @@ class AvatarHandler {
 	 * @return string
 	 */
 	public static function get_avatar_svg() {
-		return \get_user_meta( \get_current_user_id(), 'wapuugotchi_shop_svg', true );
+		return \get_user_meta( \get_current_user_id(), self::AVATAR_SVG_KEY, true );
 	}
 
 	/**
@@ -67,12 +76,28 @@ class AvatarHandler {
 	 * @return bool
 	 */
 	public static function update_avatar_svg( $svg ) {
-		\update_user_meta(
+		return self::update( self::AVATAR_SVG_KEY, $svg );
+	}
+
+	/**
+	 * Update the user meta with the given key and value.
+	 *
+	 * @param string $key The key to update.
+	 * @param mixed  $value The value to update the key with.
+	 *
+	 * @return bool
+	 */
+	private static function update( $key, $value ) {
+		$result = \update_user_meta(
 			\get_current_user_id(),
-			'wapuugotchi_shop_svg',
-			$svg
+			$key,
+			$value
 		);
 
-		return true;
+		if ( is_int( $result ) ) {
+			return true;
+		}
+
+		return $result;
 	}
 }

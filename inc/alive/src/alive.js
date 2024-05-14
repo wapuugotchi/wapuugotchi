49aa8e5e30aa2313cd9f7500683887069ac8fdbd
DerHerrFeldmann <markofeldmann.development@gmail.com>
2024-05-14 23:23:57 +0200

    eslint fixes for js

--- inc/alive/src/alive.js
+++ inc/alive/src/alive.js
@@ -1,6 +1,6 @@
 export default class Alive {
-	constructor(animations) {
-		if (!Array.isArray(animations)) {
+	constructor( animations ) {
+		if ( ! Array.isArray( animations ) ) {
 			return;
 		}
 
@@ -11,49 +11,55 @@ export default class Alive {
 	/**
 	 * Wartet, bis ein bestimmtes Element im DOM vorhanden ist.
 	 * @param {string} selector - Der CSS-Selektor des Elements, auf das gewartet wird.
-	 * @returns {Promise} - Ein Promise, das sich auflöst, wenn das Element gefunden wird.
+	 * @return {Promise} - Ein Promise, das sich auflöst, wenn das Element gefunden wird.
 	 */
-	waitForElement(selector) {
-		return new Promise((resolve) => {
+	waitForElement( selector ) {
+		return new Promise( ( resolve ) => {
 			// Überprüfen, ob das Element bereits existiert
-			const element = document.querySelector(selector);
-			if (element) {
-				resolve(element);
+			const initialElement = document.querySelector( selector );
+			if ( initialElement ) {
+				resolve( initialElement );
 				return;
 			}
 
 			// Erstellen eines MutationObservers, um das DOM zu überwachen
-			const observer = new MutationObserver((mutations, observerInstance) => {
-				mutations.forEach(() => {
-					// Überprüfen, ob das Element jetzt existiert
-					const element = document.querySelector(selector);
-					if (element) {
-						observerInstance.disconnect();
-						resolve(element);
-					}
-				});
-			});
+			const observer = new MutationObserver(
+				( mutations, observerInstance ) => {
+					mutations.forEach( () => {
+						// Überprüfen, ob das Element jetzt existiert
+						const observedElement =
+							document.querySelector( selector );
+						if ( observedElement ) {
+							observerInstance.disconnect();
+							resolve( observedElement );
+						}
+					} );
+				}
+			);
 
 			// Konfigurieren des Observers
-			observer.observe(document.body, {
+			observer.observe( document.getElementById( 'wpbody-content' ), {
 				childList: true,
-				subtree: true
-			});
-		});
+				subtree: true,
+			} );
+		} );
 	}
-
 	/**
 	 * Initialisiert die Klasse, indem sie auf das Vorhandensein des Elements wartet und dann die Animationen hinzufügt.
 	 */
 	init() {
-		this.waitForElement('#wapuugotchi_type__wapuu').then(element => {
-			this.animations.map((animation) => {
-				let style = document.createElement('style');
+		this.waitForElement( '#wapuugotchi_type__wapuu' ).then( ( element ) => {
+			this.animations.forEach( ( animation ) => {
+				const style = document.createElement( 'style' );
 				style.innerHTML = animation;
-				element.prepend(style);
-			});
-		});
+				element.prepend( style );
+			} );
+		} );
 	}
 }
 
-new Alive(wapuugotchiAnimations);
+/**
+ * wapuugotchiAnimations is set by wp_add_inline_script() in inc/alive/Manager.php
+ */
+/* global wapuugotchiAnimations */
+new Alive( wapuugotchiAnimations );

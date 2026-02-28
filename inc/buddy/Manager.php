36926c5176515dd9f892bb3eccd243deba7dfa5c
DerHerrFeldmann <markofeldmann.development@gmail.com>
2026-02-28 19:19:40 +0100

    update wapuugotchi_bubble_messages priority

--- inc/buddy/Manager.php
+++ inc/buddy/Manager.php
@@ -23,7 +23,7 @@ class Manager {
 	 * "Constructor" of this Class
 	 */
 	public function __construct() {
-		\add_filter( 'wapuugotchi_bubble_messages', array( Greeting::class, 'add_greetings_filter' ), PHP_INT_MAX, 1 );
+		\add_filter( 'wapuugotchi_bubble_messages', array( Greeting::class, 'add_greetings_filter' ), 10, 1 );
 		\add_action( 'admin_enqueue_scripts', array( $this, 'add_feed_script' ), PHP_INT_MAX );
 		\add_filter( 'wapuugotchi_bubble_messages', array( Feed::class, 'add_feed_filter' ), PHP_INT_MAX, 1 );
 	}
